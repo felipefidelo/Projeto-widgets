6ec5b56ae7cf3507a6da217f23ad5daac8e4fe9c
Felipe Fidel <80488760+felipefidelo@users.noreply.github.com>
2024-03-03 13:19:32 -0300

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -1,65 +1,23 @@
-<!-- chat-widget.html -->
-<style>
-    #chat-widget {
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        z-index: 9999;
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    var chatButton = document.getElementById("chat-button");
+    var messageBubble = document.getElementById("message-bubble");
 
-    #chat-button {
-        width: 50px;
-        height: 50px;
-        background-color: #25D366; /* Cor do botão de chat */
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-    }
+    chatButton.addEventListener("click", function() {
+        if (messageBubble.style.display === "none") {
+            messageBubble.style.display = "block";
+        } else {
+            messageBubble.style.display = "none";
+        }
+    });
+});
 
-    #message-bubble {
-        position: absolute;
-        bottom: 70px;
-        right: 60px;
-        background-color: #FFF; /* Cor de fundo da bolha de mensagem */
-        padding: 10px 15px;
-        border-radius: 10px;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-        display: none;
-        z-index: 9999;
-    }
+function redirectToWhatsApp() {
+    var phoneNumber = "SEUNUMERO"; // Substitua SEUNUMERO pelo número de telefone desejado
+    var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber;
 
-    .contact-name {
-        font-weight: bold;
-        margin-bottom: 5px;
-    }
+    // Redirecionar para o link do WhatsApp
+    window.location.href = whatsappURL;
 
-    .message {
-        font-size: 14px;
-    }
-
-    .message-button {
-        display: block;
-        width: 100%;
-        background-color: #25D366; /* Cor do botão de iniciar chat */
-        color: #FFF;
-        border: none;
-        border-radius: 5px;
-        padding: 8px 15px;
-        margin-top: 10px;
-        cursor: pointer;
-    }
-</style>
-
-<div id="chat-widget">
-    <div id="chat-button">
-        <img src="https://github.com/felipefidelo/Projeto-widgets/blob/main/Animation%20-%201709402093777.gif?raw=true" alt="Ícone de Chat" width="30">
-    </div>
-    <div id="message-bubble">
-        <div class="contact-name">André</div>
-        <div class="message">Olá, em que posso ajudar?</div>
-        <button class="message-button" onclick="redirectToWhatsApp()">Iniciar Chat</button>
-    </div>
-</div>
-<script src="https://felipefidelo.github.io/Projeto-widgets/chat-widget.js"></script>
+    // Retornar false para evitar o comportamento padrão de clicar em um link
+    return false;
+}
